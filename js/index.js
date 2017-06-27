b7dd2bfa4d36c639e7e5bcc91be204a02cab58f2
yienCheng <cye_orange@163.com>
2017-06-27 12:50:32 +0800

    creat new project
    creat new project

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/* 
+* @Author: Marte
+* @Date:   2017-05-26 11:03:33
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-06-27 12:17:47
+*/
+
+$(document).ready(function(){
+    $('.goTop').on('tap', function () {
+        $('body').scrollTop(0);
+    });
+});
